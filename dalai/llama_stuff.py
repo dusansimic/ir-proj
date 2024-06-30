66bb1dfef653e4e1e51109473b2b9a1833c8f0cd
Dušan Simić <dusan.simic1810@gmail.com>
2024-06-30 10:25:35 +0200

    fix: Make formatter happy

--- dalai/llama_stuff.py
+++ dalai/llama_stuff.py
@@ -1,16 +1,15 @@
 import re
 from llama_cpp import Llama
+
 # import pandas as pd
 
-MODEL_PATH = '/Users/vladco/.cache/huggingface/hub/models--TheBloke--Llama-2-13B-chat-GGML/snapshots/3140827b4dfcb6b562cd87ee3d7f07109b014dd0/llama-2-13b-chat.ggmlv3.q5_1.bin'
+MODEL_PATH = "/Users/vladco/.cache/huggingface/hub/models--TheBloke--Llama-2-13B-chat-GGML/snapshots/3140827b4dfcb6b562cd87ee3d7f07109b014dd0/llama-2-13b-chat.ggmlv3.q5_1.bin"
+
 
 class LlamaPrompter:
     def __init__(self, base_template):
         self.model = Llama(
-            model_path=MODEL_PATH,
-            n_threads=10,
-            n_batch=512,
-            n_gpu_layers=32
+            model_path=MODEL_PATH, n_threads=10, n_batch=512, n_gpu_layers=32
         )
         self.base_template = base_template
 
@@ -18,16 +17,18 @@ class LlamaPrompter:
         return self.base_template.format(**kwargs)
 
     def prompt_model(self, prompt):
-        response=self.model(
-            prompt=prompt, 
+        response = self.model(
+            prompt=prompt,
             max_tokens=256,
             temperature=0.5,
             top_p=0.95,
             repeat_penalty=1.2,
             top_k=150,
-            echo=True)
+            echo=True,
+        )
 
         return response
+
 
 class LlamaParser:
     def __init__(self, parse_function):
@@ -36,9 +37,10 @@ class LlamaParser:
     def parse_output(self, model_output):
         return self.parse_function(model_output)
 
+
 class LlamaSession:
     def __init__(self, paragraph):
-        self.base_template_1 = '''SYSTEM: You are a humble learning assistant trained to generate questions and provide answers based on the provided text. Be clear and concise.
+        self.base_template_1 = """SYSTEM: You are a humble learning assistant trained to generate questions and provide answers based on the provided text. Be clear and concise.
 
 USER: Generate three question and corresponding answers based on the following paragraph.
 Output should be in format: 
@@ -47,8 +49,8 @@ A: [your answer to your question]
 Context: {paragraph}
 
 ASSISTANT:
-'''
-        self.base_template_2 = '''SYSTEM: You are a humble learning assistant trained to analyze learner answer on provided text and corresponding question. Be clear and concise.
+"""
+        self.base_template_2 = """SYSTEM: You are a humble learning assistant trained to analyze learner answer on provided text and corresponding question. Be clear and concise.
 
 USER: Based on the following paragraph, question, correct answer and user answer, tell to user if his answer was correct, and very briefly explain why.
 Paragraph: {paragraph}
@@ -57,7 +59,7 @@ Correct answer: {correct_answer}
 User answer: {user_answer}
 
 ASSISTANT:
-'''
+"""
         self.prompter = LlamaPrompter(self.base_template_1)
         self.prompter_grader = LlamaPrompter(self.base_template_2)
         self.parser_questions_answers = LlamaParser(self.parse_questions_answers)
@@ -71,19 +73,23 @@ ASSISTANT:
         # print(prompt)
         raw_output = self.prompter.prompt_model(prompt)
         # print(raw_output)
-        self.questions, self.answers = self.parser_questions_answers.parse_output(raw_output['choices'][0]['text'])
+        self.questions, self.answers = self.parser_questions_answers.parse_output(
+            raw_output["choices"][0]["text"]
+        )
 
     def receive_answers(self, user_answers, idx):
         prompt = self.prompter_grader.prepare_prompt(
             paragraph=self.paragraph,
             question=self.questions[idx],
             correct_answer=self.answers[idx],
-            user_answer=user_answers
+            user_answer=user_answers,
         )
         # print('prompt:', prompt, '\n')
         raw_output = self.prompter_grader.prompt_model(prompt)
         # print(raw_output['choices'][0]['text'])
-        grading_result = self.parser_grades.parse_output(raw_output['choices'][0]['text'])
+        grading_result = self.parser_grades.parse_output(
+            raw_output["choices"][0]["text"]
+        )
 
         return grading_result
 
@@ -96,7 +102,7 @@ ASSISTANT:
 
     @staticmethod
     def parse_grades(model_output):
-        return model_output.split('ASSISTANT:')[1].strip()
+        return model_output.split("ASSISTANT:")[1].strip()
 
     def get_results(self):
         return self.questions, self.answers
